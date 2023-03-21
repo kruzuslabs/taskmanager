29eea142bd3392a3af590464c456193cec2736dd
Bek <kruzus@live.com>
2023-03-21 00:07:46 -0400

    lol

--- src/main/java/com/kruzus/taskmanager/TaskmanagerApplication.java
+++ src/main/java/com/kruzus/taskmanager/TaskmanagerApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskmanagerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TaskmanagerApplication.class, args);
     }
-
-
 }
